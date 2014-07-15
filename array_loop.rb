d255060407605f9504144d49eb2923ee06662ff0
Roy M J <roymj88@gmail.com>
2014-07-15 15:16:30 +0530

    For Loop

--- array_loop.rb
+++ array_loop.rb
@@ -2,6 +2,9 @@
 array = [1,2,3,4,5,6,7,8,9];
 odd = []; even = []; odd1 = []; even1 =[]; odd2 = []; even2 =[];odd3 = []; even3 = [];odd4 = []; even4 = [];
 
+#Printing Main Array
+puts "Main Array : "+ "#{array.inspect}";
+
 #Method 1 - Using Each Loop
 array.each do|i|
     if(i%2==0)
@@ -11,12 +14,8 @@ array.each do|i|
     end
 end
 puts "First Method using 'each'";
-puts "Main Array : "+ "#{array.inspect}";
 puts "Odd : " + "#{odd.inspect}";
 puts "Even : "+ "#{even.inspect}";
- 
-
-
  
 #Method 2 - Using Collect Loop
 array.collect do|i|
@@ -24,6 +23,17 @@ array.collect do|i|
     odd1 << i unless !(i%2==0);
 end
 puts "Second Method using 'collect'";
-puts "Main Array : "+ "#{array.inspect}";
 puts "Odd : " + "#{odd1.inspect}";
 puts "Even : "+ "#{even1.inspect}";
+
+#Method 3 - Using For Loop
+for key in array
+    if(key%2==0)
+        even2 << key;
+    elsif(key%2==1)
+        odd2 << key;
+    end
+end 
+puts "Third Method using 'for'";
+puts "Odd : " + "#{odd2.inspect}";
+puts "Even : "+ "#{even2.inspect}";
