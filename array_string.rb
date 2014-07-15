47b76a044e5e93bae76e5e29931b36a2dbfd91a9
Roy M J <roymj88@gmail.com>
2014-07-15 15:30:17 +0530

    Reverse an Array

--- array_string.rb
+++ array_string.rb
@@ -2,3 +2,8 @@
 array      = ["Ruby", "is", "a", "wonderful", "language"];
 array_new  = array.join(" ")+ ".";
 puts array_new;
+
+#Reverse an array and convert to string
+reversedString  = array.reverse;
+reversed_new    = reversedString.join(" ")+ ".";
+puts reversed_new;
