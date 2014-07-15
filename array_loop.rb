494b2a4489fd32b9773ba49763ceef5330f43f85
Roy M J <roymj88@gmail.com>
2014-07-15 15:09:31 +0530

    Each methodf

---
+++ array_loop.rb
@@ -0,0 +1,14 @@
+array = [1,2,3,4,5,6,7,8,9];
+odd = []; even = []; odd1 = []; even1 =[]; odd2 = []; even2 =[];odd3 = []; even3 = [];odd4 = []; even4 = [];
+array.each do|i|
+    if(i%2==0)
+        even << i;
+    elsif (i%2==1)
+        odd << i;
+    end
+end
+puts "First Method using 'each'";
+puts "Main Array : "+ "#{array.inspect}";
+puts "Odd : " + "#{odd.inspect}";
+puts "Even : "+ "#{even.inspect}";
+ 
