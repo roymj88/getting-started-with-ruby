c86d09111f438f504f4c470f2dcc81e20de0ade1
Roy M J <roymj88@gmail.com>
2014-07-16 10:58:03 +0530

    Modification

--- directory_file.rb
+++ directory_file.rb
@@ -2,8 +2,7 @@
 require 'active_support/core_ext/integer/inflections'
 if !File.directory?("test")
     Dir.mkdir("test")
-end
- 
+end 
 Dir.glob('test/*.log') do |log_file|
     contents = File.read(log_file)
     contents = contents.gsub(/[0123456789]/, 'z')
@@ -14,4 +13,19 @@ Dir.glob('test/*.log') do |log_file|
             file.write("Date of creation of the file : #{t.strftime('%A, %B')}, #{t.day.ordinalize}, #{t.year}  \n")
         }
     }
+end
+
+#Method 2
+if !File.directory?("Test")
+    Dir.mkdir("Test"); # Create Directory
+end
+Dir.chdir("Test")
+list = Dir.glob("*\.log")
+list.each do |data|  
+    File.open("data.log", "a") { 
+        remote_data = open(data).read;
+        "HELLO WORLD HOW ARE YOU".gsub(/\w+/) do |word|
+            puts remote_data = word.capitalize;
+        end
+    }       
 end
